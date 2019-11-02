562dfb667833c1debbc33d292d48198e7833ff18
Mark Linton <46918217+Mark-Linton@users.noreply.github.com>
2019-11-02 00:08:44 -0700

    Initial code commit

---
+++ BrewBuddy/Controllers/RecipesController.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="RecipesController.cs" company="Mark Linton">
+// Copyright (c) Mark Linton. All rights reserved.
+// Licensed under the GNU GENERAL PUBLIC LICENSE Version 3 license.
+// See LICENSE file in the solution root for full license information.
+// </copyright>
+
+namespace BrewBuddy.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipesController : ControllerBase
+    {
+    }
+}
---
+++ BrewBuddy/Program.cs
@@ -0,0 +1,29 @@
+// <copyright file="Program.cs" company="Mark Linton">
+// Copyright (c) Mark Linton. All rights reserved.
+// Licensed under the GNU GENERAL PUBLIC LICENSE Version 3 license.
+// See LICENSE file in the solution root for full license information.
+// </copyright>
+
+namespace BrewBuddy
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
